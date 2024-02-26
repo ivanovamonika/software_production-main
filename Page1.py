0c60fe7c627f3215e859416220aa891b1035f933
ivanovamonika <71437821+ivanovamonika@users.noreply.github.com>
2024-02-26 16:23:59 +0200

    Update Page1.py
    final change

--- Page1.py
+++ Page1.py
@@ -28,10 +28,9 @@ class Page1(tk.Frame):
   #############
 
   def selectFile(self):
-    # Example of how to open a file dialog and get the selected file path
+    #Opening a file dialog and get the selected file path
     self.app.selectedFilePath = ctk.filedialog.askopenfile(title="Open File", filetypes=(("Open a .csv file", "*.csv"), ("All files", "*.*"))).name
-    # Example of how to update text for a label
-    self.labelSelectedFile.configure(text = f"We have selected a file: {self.app.selectedFilePath}")
+
     
   def proceed(self):
     # Check if a file has been selected
@@ -39,24 +38,22 @@ class Page1(tk.Frame):
       # Show a warning message
       tk.messagebox.showwarning("No File Selected", "Please select a CSV file before continuing.")
       return
-    # Here you could now validate the file and then add any state variables to the app which page 2 or other pages can then access
-    # Like this: self.app.someThing = "someValue"
-    # Then in the page 2, you could access self.app.someThing and it would contain that same value
+    
     # Read the CSV file using pandas
     df = pd.read_csv(self.app.selectedFilePath)
 
-    # Get the column names
+    # Get and save the column names into a list and save them for future use
     self.app.columnNames = (df.columns).tolist()
     for i in range(len(self.app.columnNames)):
       self.app.columnNames[i] = self.app.columnNames[i].replace("_", " ")
+    #check to see if it worked
     print(self.app.columnNames)
 
-    # Example of how to switch pages
+    #switching to page 2
     self.app.show_page(2)
 
   def on_show(self):
     print("Showing page 1")
-    #self.combo.configure(values=self.app.columnNames)
 
   
       
@@ -70,11 +67,10 @@ class Page1(tk.Frame):
     NORMALFONT = app.styles.get("NORMALFONT")
     LARGEFONT = app.styles.get("LARGEFONT")
     EXTRALARGEFONT = app.styles.get("EXTRALARGEFONT")
-    MIDDLEFONT = app.styles.get("MIDDLEFONT")
     MINIFONT = app.styles.get('MINIFONT')
 
   
-    # Label for description
+    # Title of app label
     # https://github.com/TomSchimansky/CustomTkinter/wiki/CTkLabel
     self.labelTitle = ctk.CTkLabel(master=self, font = EXTRALARGEFONT, text="Homestimator")
     self.labelTitle.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
@@ -87,6 +83,8 @@ class Page1(tk.Frame):
                                           font = LARGEFONT)
     self.subTitle.place(relx=0.5, rely=0.28, anchor=tk.CENTER)
     
+
+    #button
     
     self.frameForButtons = ctk.CTkFrame(master=self, width=300, height=300, corner_radius=30, bg_color='#A6BF93')
     self.frameForButtons.place(relx=0.5, rely=0.65, anchor = tk.CENTER)
@@ -101,60 +99,9 @@ class Page1(tk.Frame):
     self.buttonContinue = ctk.CTkButton(master=self.frameForButtons, text="Continue", command= lambda : self.proceed(), fg_color='#90658B',text_color='#FFFFFF', font=NORMALFONT, hover_color='#361632')
     self.buttonContinue.place(relx=0.5, rely=0.60, anchor=tk.CENTER)
 
-    self.labelSelectedFile = ctk.CTkLabel(master=self, text='',font=MINIFONT, fg_color='transparent')
-    self.labelSelectedFile.place(relx=0.5, rely=0.37, anchor=tk.CENTER)
-
     #copyright label
     self.copyRightLabel = ctk.CTkLabel(master=self,text = " copy rights SApp Wizz c", 
                                           font = MINIFONT)
     self.copyRightLabel.place(relx=0.5, rely=0.98, anchor=tk.CENTER)
   
       
-  #######################
-  # INITIALIZE THE PAGE #
-  #######################
-  def __init__(self, parent, app):
-    tk.Frame.__init__(self, parent, bg='#A6BF93')
-    self.app = app
-
-    NORMALFONT = app.styles.get("NORMALFONT")
-    LARGEFONT = app.styles.get("LARGEFONT")
-    EXTRALARGEFONT = app.styles.get("EXTRALARGEFONT")
-    MIDDLEFONT = app.styles.get("MIDDLEFONT")
-    MINIFONT = app.styles.get('MINIFONT')
-
-  
-    # Label for description
-    # https://github.com/TomSchimansky/CustomTkinter/wiki/CTkLabel
-    self.labelTitle = ctk.CTkLabel(master=self, font = EXTRALARGEFONT, text="Homestimator")
-    self.labelTitle.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
-
-    # Label for selected file
-    # https://github.com/TomSchimansky/CustomTkinter/wiki/CTkLabel    
-    self.subTitle= ctk.CTkLabel(master=self, 
-                  
-                                          text = " Upload CSV file\n with house data", 
-                                          font = LARGEFONT)
-    self.subTitle.place(relx=0.5, rely=0.28, anchor=tk.CENTER)
-    
-    
-    self.frameForButtons = ctk.CTkFrame(master=self, width=300, height=300, corner_radius=30, bg_color='#A6BF93')
-    self.frameForButtons.place(relx=0.5, rely=0.65, anchor = tk.CENTER)
-
-    # Button to select a file, calls the selectFile function inside this class
-    # https://github.com/TomSchimansky/CustomTkinter/wiki/CTkButton
-    self.buttonSelectFile = ctk.CTkButton(master=self.frameForButtons, text="Select CSV File", command = lambda : self.selectFile(), fg_color='#90658B', text_color='#FFFFFF', compound='top', font=NORMALFONT, hover_color='#361632')
-    self.buttonSelectFile.place(relx=0.5, rely=0.40, anchor=tk.CENTER)
-
-    # Button to continue to the next page, calls the proceed function inside this class
-    # https://github.com/TomSchimansky/CustomTkinter/wiki/CTkButton
-    self.buttonContinue = ctk.CTkButton(master=self.frameForButtons, text="Continue", command= lambda : self.proceed(), fg_color='#90658B',text_color='#FFFFFF', font=NORMALFONT, hover_color='#361632')
-    self.buttonContinue.place(relx=0.5, rely=0.60, anchor=tk.CENTER)
-
-    self.labelSelectedFile = ctk.CTkLabel(master=self, text='',font=MINIFONT, fg_color='transparent')
-    self.labelSelectedFile.place(relx=0.5, rely=0.37, anchor=tk.CENTER)
-
-    #copyright label
-    self.copyRightLabel = ctk.CTkLabel(master=self,text = " copy rights SApp Wizz c", 
-                                          font = MINIFONT)
-    self.copyRightLabel.place(relx=0.5, rely=0.98, anchor=tk.CENTER)
